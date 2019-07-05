9183af6e69274704fcecfc8c631070e107bee17a
oliverjam <oliverjamesphillips@gmail.com>
2019-07-05 09:26:50 +0100

    Change routes into templates

--- router.mjs
+++ router.mjs
@@ -1,22 +1,22 @@
 import { parse } from "url";
-import handlers from "./handlers";
+import templates from "./templates";
 
 async function router(request, response) {
   try {
     const url = parse(request.url, true);
     const path = url.pathname === "/" ? "home" : url.pathname.replace("/", "");
-    const routeTemplate = handlers[path];
-    if (!routeTemplate) {
+    const routeHandler = templates[path];
+    if (!routeHandler) {
       const error = new Error(`/${path} not found`);
       error.status = 404;
       throw error;
     }
-    const html = await routeTemplate({ url });
+    const html = await routeHandler({ url });
     response.writeHead(200, { "content-type": "text/html" });
     response.end(html);
     console.log(200, `/${path}`);
   } catch (error) {
-    const html = handlers.missing({ message: error.message });
+    const html = templates.missing({ message: error.message });
     response.writeHead(error.status || 500, { "content-type": "text/html" });
     response.end(html);
     console.error(error.status, error.message);
